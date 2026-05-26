71ccf18192d3e50eb4606a7ed71efb8a660dd5dd
kenura-krag <kenura@krag.lk>
2026-05-26 12:00:35 +0530

    done

--- src/drivers/comparator.h
+++ src/drivers/comparator.h
@@ -1,3 +1,4 @@
+// comparator.h
 #pragma once
 
 #include <avr/io.h>
@@ -40,20 +41,7 @@ static inline void comp_use_bandgap(uint8_t enable) {
 
 // Configure and enable the comparator hardware interrupt
 static inline void comp_interrupt_enable(uint8_t trigger_mode) {
-    // 1. Disable interrupt temporarily while changing settings
-    ACSR &= ~(1 << ACIE);
-
-    // 2. Clear the old trigger mode bits (ACIS1 and ACIS0)
-    ACSR &= ~((1 << ACIS1) | (1 << ACIS0));
-
-    // 3. Set the new trigger mode
     ACSR |= (trigger_mode << ACIS0);
-
-    // 4. Clear any pending interrupt flags (write 1 to ACI to clear it!)
-    ACSR |= (1 << ACI);
-
-    // 5. Re-enable the interrupt
-    ACSR |= (1 << ACIE);
 }
 
 // Disable the comparator interrupt
@@ -63,16 +51,16 @@ static inline void comp_interrupt_disable(void) {
 
 // Route the Negative (-) input to an Analog Pin (PC0 - PC5) instead of PD7
 static inline void comp_use_adc_pin(uint8_t adc_channel) {
-    // 1. MUST turn off the ADC hardware to borrow its multiplexer!
+    // MUST turn off the ADC hardware to borrow its multiplexer!
     ADCSRA &= ~(1 << ADEN);
 
-    // 2. Turn on the bridge between the ADC MUX and the Comparator
+    // Turn on the bridge between the ADC MUX and the Comparator
     ADCSRB |= (1 << ACME);
 
-    // 3. Clear the old pin selection in the MUX (bits 2, 1, 0)
+    // Clear the old pin selection in the MUX (bits 2, 1, 0)
     ADMUX &= ~0x07;
 
-    // 4. Set the new pin!
+    // Set the new pin!
     // (e.g., 5 = PC5, 4 = PC4, 3 = PC3)
     ADMUX |= (adc_channel & 0x07);
 }
--- src/main.c
+++ src/main.c
@@ -1,30 +1,21 @@
+// main.c
+
 #include "drivers/millis.h"
 #include "drivers/gpio.h"
 #include "drivers/display.h"
-#include "drivers/scroll.h"
 #include "drivers/comparator.h"
-
-// use a volatile variable so the ISR can talk to the main loop
-volatile uint8_t comparator_triggered = 0;
-
-// This is the Interrupt Service Routine.
-// The hardware calls this function INSTANTLY when the comparator triggers!
-ISR(ANALOG_COMP_vect) {
-    comparator_triggered = 1;
-}
 
 int main(void) {
     gpio_set_all_output();
     millis_init();
-    // Set your pins as inputs
 
     // Setup the Comparator
     comp_enable();
 
-    // Tell comparator to use Internal 1.1V (Frees up PD6!)
+    // Set the comparator to use Internal 1.1V (Frees up PD6!)
     comp_use_bandgap(1);
 
-    // Tell the comparator to use PC5 instead of PD7!
+    // Set the comparator to use PC5 instead of PD7!
     comp_use_adc_pin(5);
 
     // Set it to trigger an interrupt when the sensor voltage drops
@@ -40,19 +31,8 @@ int main(void) {
 
     for (;;) {
         display_refresh();
-        // scroll_tick();
 
-        uint32_t now = millis();
-        // uint8_t current_sw_state = gpio_read(TOGGLE_SW);
-
-        // Check if the hardware interrupt caught a trigger
         if (comp_read()) {
-
-            // 1. Reset the flag immediately
-            comparator_triggered = 0;
-
-            // 2. Do your reaction timer logic here!
-            // current_window = WINDOW_MEASURING;
             gpio_write(BUZZER, HIGH);
             display_write_str("HIGH");
         } else{
